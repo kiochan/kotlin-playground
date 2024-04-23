af17ffee648ab77e567e356a028cc69bf5fa22b4
Xingchen Sun <xingchen.sun@rhenag.de>
2024-04-23 13:32:28 +0200

    refactor

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -1,13 +1,15 @@
 class Config(private val file: String) {
     // `object{}.javaClass.getResourceAsStream` works too
     // `ResourceBundle.getBundle("Messages", locale)` also works
-    val map: Map<String, String> = this::class.java.getResourceAsStream(file)
-        ?.bufferedReader()?.use { reader ->
-            reader.lineSequence()
-                .map { it.split("=") }
-                .filter { parts -> parts.size == 2 }
-                .map { (key, value) -> key.trim() to value.trim() }
-                .filter { (key, value) -> key.isNotEmpty() && value.isNotEmpty() }
-                .toMap()
-        } ?: HashMap<String,String>()
-}
+        val map: Map<String, String> by lazy {
+            this::class.java.getResourceAsStream(file)?.use { stream ->
+                stream.bufferedReader().useLines { lines ->
+                    lines.map { it.split("=") }
+                        .filter { it.size == 2 }
+                        .map { (key, value) -> key.trim() to value.trim() }
+                        .filter { (key, value) -> key.isNotEmpty() && value.isNotEmpty() }
+                        .toMap()
+                }
+            } ?: emptyMap()
+        }
+    }
